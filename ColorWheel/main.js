a189ffb15fa7cf3f97d683b3f30d6515ed1a84b7
Juan Aguirre <jaguirr10@gmail.com>
2018-06-18 21:02:58 -0400

    Add files via upload

---
+++ ColorWheel/main.js
@@ -0,0 +1,15 @@
+   // Juan Aguirre
+
+   for (var i=0; i<360; i++) {
+
+			var color = document.createElement("span")
+			color.setAttribute("id", "d" + i)
+			color.style.backgroundColor = "hsl(" + i + ", 100%, 50%)"
+			color.style.msTransform = "rotate(" + i + "deg)"
+			color.style.webkitTransform = "rotate(" + i + "deg)"
+			color.style.MozTransform = "rotate(" + i + "deg)"
+			color.style.OTransform = "rotate(" + i + "deg)"
+			color.style.transform = "rotate(" + i + "deg)"
+			document.getElementById('colorwheel').appendChild(color)
+		};
+
